e1eb9f1f438de1bde68194598cffd041bc9a3705
dantebenicio <dantecosta79@gmail.com>
2022-04-03 18:34:24 -0300

    Card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,38 @@
+import { useContext } from 'react';
+import { AppContext } from '../../context';
+import { CardProps } from '../../types/card';
+import Button from '../Button';
+import DaysLeft from '../DaysLeft';
+import { Container, Wrapper } from './styles';
+
+export default function Card({
+  name, pledgePrice, description, daysLeft, outOfStock,
+}: CardProps) {
+  const { setShowThanksModal } = useContext(AppContext);
+
+  return (
+    <Container outOfStock={outOfStock!}>
+      <Wrapper>
+        <div className="wrapper">
+          <h3>{name}</h3>
+          <span id="pledge_price">
+            Pledge $
+            {pledgePrice}
+            {' '}
+            or more
+          </span>
+        </div>
+        <p>{description}</p>
+        <div className="wrapper">
+          <DaysLeft days={daysLeft} fontSize={28} smallFontSize={15} />
+
+          <Button
+            text="Select Reward"
+            width={160}
+            showModalFn={setShowThanksModal}
+          />
+        </div>
+      </Wrapper>
+    </Container>
+  );
+}
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+type ContainerProps = {
+  outOfStock: boolean
+}
+
+export const Container = styled('div')<ContainerProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+
+  padding: 2rem;
+
+  border: 1px solid ${({ theme }) => theme.colors.neutral.lightGray};
+  border-radius: ${({ theme }) => theme.pixelToRem(8)};
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  gap: 2rem;
+
+  > p {
+    color: ${({ theme }) => theme.colors.neutral.darkGray};
+    font-size: clamp(0.8rem, 5vw, 1rem);
+    font-weight: ${({ theme }) => theme.fontWeights.normal};
+  }
+
+  > .wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+
+    > #pledge_price {
+      color: ${({ theme }) => theme.colors.primary.moderateCyan};
+      font-weight: ${({ theme }) => theme.fontWeights.medium};
+      font-size: clamp(0.8rem, 10vw, 1rem);
+
+      cursor: pointer;
+
+      transition: color 0.2s ease;
+
+      &:hover {
+        color: ${({ theme }) => theme.colors.primary.darkCyan};
+      }
+    }
+  }
+`;
