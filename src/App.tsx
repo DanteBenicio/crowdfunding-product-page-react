ad47d1c53019f7d3bdb9884e3c51a7134ef7a3cc
dantebenicio <dantecosta79@gmail.com>
2022-04-05 10:20:04 -0300

    render application modals

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import Navbar from './components/Navbar';
 import FirstSection from './components/FirstSection';
 import MainSection from './components/MainSection';
+import { AppContext } from './context';
+import ThanksModal from './components/ThanksModal';
+import BackThisProjectModal from './components/BackThisProjectModal';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <>
-      <Navbar />
+      {showThanksModal && <ThanksModal />}
+      {showBackThisProjectModal && <BackThisProjectModal />}
+      {openBurger && <Menu />}
+      <Navbar openBurger={openBurger} setOpenBurger={setOpenBurger} />
       <FirstSection />
       <MainSection />
     </>
