276901f67b2ea65c4b9b3efdf8c56ce48ff661c8
dantebenicio <dantecosta79@gmail.com>
2022-04-05 10:18:53 -0300

    add fixedWidth and notResize prop

--- src/types/button.ts
+++ src/types/button.ts
@@ -3,6 +3,8 @@ import React from 'react';
 export type ButtonProps = {
   text: string
   width?: number
+  fixedWidth?: number
+  notResize?: boolean
   showModalFn?: React.Dispatch<React.SetStateAction<boolean>>
   showThanksModal?: boolean
   showBackThisProjectModal?: boolean
