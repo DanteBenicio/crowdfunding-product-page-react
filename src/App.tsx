c53d8d51a95d7a519c7b7c0a7f0f133f4b4fef8e
dantebenicio <dantecosta79@gmail.com>
2022-04-05 10:20:51 -0300

    application effects

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,31 @@ import BackThisProjectModal from './components/BackThisProjectModal';
 import Menu from './components/Menu';
 
 function App() {
+  const {
+    showThanksModal, showBackThisProjectModal,
+  } = useContext(AppContext);
+  const [openBurger, setOpenBurger] = useState<boolean>(false);
+
+  useEffect(() => {
+    const body = document.querySelector('body');
+
+    window.addEventListener('resize', (e) => {
+      const { innerWidth } = e.target as Window;
+
+      if (innerWidth >= 768) {
+        setOpenBurger(false);
+      }
+    });
+
+    if (showBackThisProjectModal) {
+      body!.style.overflowY = 'hidden';
+    } else {
+      body!.style.overflowY = 'scroll';
+    }
+
+    return () => window.removeEventListener('resize', () => {});
+  }, [showBackThisProjectModal]);
+
   return (
     <>
       {showThanksModal && <ThanksModal />}
