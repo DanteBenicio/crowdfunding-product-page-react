4caa3ca951380ca5a8b96fb1ddffd0ff2e74b443
dantebenicio <dantecosta79@gmail.com>
2022-04-05 10:22:39 -0300

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,9 @@ module.exports = {
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
     'react/react-in-jsx-scope': 'off',
+    'max-len': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
   },
 };
