2dccf6340c115b3eedf846d17547062b18d18acc
dantebenicio <dantecosta79@gmail.com>
2022-04-04 22:33:11 -0300

    Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,37 @@
+import { ButtonProps } from '../../types/button';
 import { ButtonElement } from './styles';
 
-type ButtonProps = {
-  text: string
-}
+export default function Button({
+  text, width, showModalFn, notResize, fixedWidth,
+}: ButtonProps) {
+  if (showModalFn) {
+    return (
+      <ButtonElement
+        type="button"
+        width={width!}
+        notResize={notResize!}
+        onClick={() => showModalFn?.((prevState) => !prevState)}
+        fixedWidth={fixedWidth!}
+      >
+        {text}
+      </ButtonElement>
+    );
+  }
 
-export default function Button({ text }: ButtonProps) {
   return (
-    <ButtonElement type="button">{text}</ButtonElement>
+    <ButtonElement
+      type="button"
+      width={width!}
+      notResize={notResize!}
+      fixedWidth={fixedWidth!}
+    >
+      {text}
+    </ButtonElement>
   );
 }
+
+Button.defaultProps = {
+  width: 0,
+  setShowThanksModal: () => {},
+  showThanksModal: false,
+};
